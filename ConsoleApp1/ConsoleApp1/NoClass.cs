ec6a8b8657c65def7185450def51a276463d321b
wiggecom <wigge@hotmail.com>
2023-10-11 11:49:05 +0200

    Called NoClass in main
    Pulling some info from NoClass-class

--- ConsoleApp1/ConsoleApp1/NoClass.cs
+++ ConsoleApp1/ConsoleApp1/NoClass.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     internal class NoClass
     {
+        public static void Classy()
+        {
+        Console.WriteLine("Unfortunately no class");
+        }
     }
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Tjoflöjt!");
+            NoClass.Classy();
         }
     }
 }
